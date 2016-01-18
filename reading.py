d694d19c063378bc656731c63a109ecb41430ff2
ileana co <ileanalouise.co@gmail.com>
2016-01-17 16:22:31 -0800

    Initial commit for ecoscope

--- reading.py
+++ reading.py
@@ -1,10 +1,23 @@
 import sys
 import numpy
-def main():
-    script = sys.argv[0]
-    filename = sys.argv[1]
-    data = numpy.loadtxt(filename, delimiter=',')
-    for m in data.mean(axis=1):
-        print(m)
+
+def main():    
+	script = sys.argv[0]
+	action = sys.argv[1]
+	filename = sys.argv[2:]
+
+	for filename in filenames:
+	data = numpy.loadtxt(filename, delimiter= ',')
+		if action == '--min'
+			values = data.min(axis=1)
+        	elif action == '--max'
+                	values = data.max(axis=1)
+        	elif action == '--mean'
+                	values = data.mean(axis=1)
+		for m in values:
+                	print(m)
 
 main()
+	
+  
+
--- reading2.py
+++ reading2.py
@@ -7,17 +7,21 @@ def main():
     filenames = sys.argv[2:]
 
     for filename in filenames:
+            process(filename, action)
+
+def process(filename, action):
         data = numpy.loadtxt(filename, delimiter=',')
+    if action == '--min':
+        values = data.min(axis=1)
+    elif action == '--max':
+        values = data.max(axis=1)
+    elif action == '--mean':
+        values = data.mean(axis=1)
 
-        if action == '--min':
-                values = data.min(axis=1)
-        elif action == '--max':
-                values = data.max(axis=1)
-        elif action == '--mean':
-                values = data.mean(axis=1)
-
-        for m in values:
-                print(m)
+    print(filename)
+    for m in values:
+        print(m)
 
 main()
+
 
